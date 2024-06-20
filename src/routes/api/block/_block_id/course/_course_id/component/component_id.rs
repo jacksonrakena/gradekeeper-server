e0721aac29c32d8c19dd2ad36dd300dda5c7968d
jacksonrakena <jackson@rakena.co.nz>
2024-06-20 16:52:10 +1200

    Remove old sequence_number changeset option

--- src/routes/api/block/_block_id/course/_course_id/component/component_id.rs
+++ src/routes/api/block/_block_id/course/_course_id/component/component_id.rs
@@ -29,8 +29,7 @@ pub struct UpdateCourseComponentChangeset {
     pub name_of_subcomponent_singular: Option<String>,
     pub subject_weighting: Option<BigDecimal>,
     #[serde(rename = "numberOfSubComponentsToDrop_Lowest")]
-    pub number_of_subcomponents_to_drop_lowest: Option<i32>,
-    pub sequence_number: Option<i16>
+    pub number_of_subcomponents_to_drop_lowest: Option<i32>
 }
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
